b48d73d5d74daeccf273f470c97b73fced3aea7d
mloning <21020482+mloning@users.noreply.github.com>
2024-02-18 23:08:37 +0100

    add dealer struct

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,8 @@ pub fn run() {
     let players = generate_players();
     println!("{:?}", players);
 
-    let cards = generate_deck();
-    println!("{:?}", cards);
+    let dealer = Dealer::build();
+    println!("{:?}", dealer.stack);
 }
 
 fn generate_players() -> Vec<Player> {
@@ -34,6 +34,7 @@ enum Color {
 
 fn generate_deck() -> Vec<Card> {
     println!("Generating deck ...");
+    // TODO shuffle deck
     // TODO use generator to generate numbers
     // TODO use enums for symbols?
     let numbers = [
@@ -102,16 +103,53 @@ impl Player {
     }
 }
 
+// define dealer object, to handle interactions between
+#[derive(Debug)]
+struct Dealer {
+    stack: Vec<Card>,
+    pile: Vec<Card>,
+}
+
+impl Dealer {
+    fn build() -> Dealer {
+        // TODO flip first card from deck to initialize pile
+        let stack = generate_deck();
+        let pile: Vec<Card> = vec![];
+        Dealer { stack, pile }
+    }
+
+    /// Draw `n` cards from stack
+    fn draw(&mut self, n: usize) -> Vec<Card> {
+        let m = self.stack.len() - n;
+        let range = m..;
+        let cards = self.stack.drain(range).collect();
+        cards
+    }
+}
+
 // define test module, annotated with cfg attribute for conditional compilation,
-// excluding tests when building the package
+// which excludes tests when building the package
 #[cfg(test)]
 mod tests {
     use super::*; // bring private functions into scope
+    use rstest::rstest;
 
     #[test]
     fn test_generate_deck_number_of_cards() {
         let deck = generate_deck();
         let n_cards = deck.len();
         assert_eq!(n_cards, 108);
+    }
+
+    #[rstest]
+    #[case(0)]
+    #[case(1)]
+    #[case(3)]
+    #[case(7)]
+    #[case(13)]
+    fn test_dealer_draw_number_of_cards(#[case] n: usize) {
+        let mut dealer = Dealer::build();
+        let cards = dealer.draw(n);
+        assert_eq!(cards.len(), n);
     }
 }
